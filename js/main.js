6939a61691c1fa1a04df0313fcb2ef6ec7b35833
reoyan2525 <145842020+reoyan2525@users.noreply.github.com>
2024-08-11 09:42:19 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 
 $(function () {
     var loading = $("#js-loading");
+
     //ページの読み込みが完了後にアニメーションを非表示・MVの高さを調整
     $(window).on("load", function () {
         var windowHeight = $(window).height();
         $(".mv").height(windowHeight);
         loading.delay("1000").fadeOut("2000");
     });
+
+    //ページの読み込みが完了してなくても3秒後にアニメーションを非表示にする
+    setTimeout(function () {
+        loading.fadeOut("3000");
+    }, 8000);
+
+
 
     //画面のリサイズ時にmvの高さをveiwに合わせる処理
     $(window).resize(function () {
